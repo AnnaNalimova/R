ecf52da1b64e45248304d381c6d9ecd88aa542d9
Anna Nalimova <avnalimova@mail.ru>
2023-03-29 10:37:22 +0300

    64

---
+++ 64/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Задайте значение N. Напишите программу ,
+//которая выведет все натуральные числа в промежутке от N до 1 
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+int m = 1;
+Console.WriteLine(GetNumbers(m, n));
+
+string GetNumbers(int start, int end)
+{
+    if (end == start) 
+    return end.ToString();
+    return (end + " , " + GetNumbers(start, end - 1));
+}
